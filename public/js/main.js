dca9027be7e605c856c300712e5355a5b3b6745f
rob <robert_regan86@yahoo.com>
2022-01-09 16:53:28 -0500

    ps4 chat app working

---
+++ public/js/main.js
@@ -0,0 +1,73 @@
+
+const socket = io();
+const chatForm = document.getElementById('chat-form')
+const chatMessages = document.querySelector('.chat-messages')
+const roomName = document.getElementById('room-name')
+const userList = document.getElementById('users')
+// get username and room from Url
+const { username, room } = Qs.parse(location.search, {
+    ignoreQueryPrefix: true
+})
+
+// join chatroom
+socket.emit('joinRoom', {username, room})
+
+// get room and users
+socket.on('roomUsers', ({room, users})=>{
+    outputRoomName(room)
+    outputUsers(users)
+})
+
+
+
+// message from server
+socket.on('message', message =>{
+    console.log(message)
+    outputMessage(message)
+
+
+    // Scroll down to present
+    chatMessages.scrollTop = chatMessages.scrollHeight;
+})
+
+
+// Messge submit
+chatForm.addEventListener('submit', e =>{
+    e.preventDefault(); // when you submit a form it automatically submits to a file, this will stop that behavior
+
+
+    const msg = e.target.elements.msg.value // gives us the current element using msg as the ID
+
+// emit message to server
+    socket.emit('chatMessage', msg)
+
+    // clears input and focuses on it
+    e.target.elements.msg.value = ''
+    e.target.elements.msg.focus()
+})
+
+
+// output message to DOM
+
+function outputMessage(message) {
+    const div = document.createElement('div')
+    div.classList.add('message')
+    div.innerHTML = `<p class="meta">${message.username}<span> ${message.time}</span></p>
+    <p class="text">
+       ${message.text}
+    </p>`;
+    document.querySelector('.chat-messages').appendChild(div)
+
+}
+
+// add room name to DOM 
+function outputRoomName(room){
+    roomName.innerText = room
+}
+
+// add users to DOM 
+function outputUsers(users){
+    userList.innerHTML = `
+    ${users.map(user => `<li>${user.username}</li>`).join('')}
+    `
+}
---
+++ server.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const socketio = require('socket.io');
+const formatMessage = require('./utils/messages')
+const { userJoin, getCurrentUser, userLeave, getRoomUsers } = require('./utils/users')
+
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+//set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+const botName = 'good Bot'
+
+// Run when client connects
+io.on('connection', socket =>{
+    socket.on('joinRoom', ({ username, room }) => {
+        const user = userJoin( socket.id, username, room )
+        socket.join(user.room)
+// welcome current user
+socket.emit('message', formatMessage(botName, 'Welcome to the Chat!'));
+
+// Broadcase when a user connects - sends message to everyone but the user that is connecting
+socket.broadcast
+    .to(user.room)
+    .emit('message',  formatMessage(botName, `${user.username} has joined the chat`));
+
+    // Send users and room info to sidebar
+        io.to(user.room).emit('roomUsers', {
+            room: user.room,
+            users: getRoomUsers(user.room)
+        })
+    })
+    // Listen for chatMessage
+    socket.on('chatMessage', msg =>{
+        const user = getCurrentUser(socket.id)
+
+        io.to(user.room).emit('message', formatMessage(user.username, msg))
+    })
+// Runs when client Disconnects (needs to be inside connection)
+socket.on('disconnect', () =>{
+    const user = userLeave(socket.id)
+    if(user){
+        io.to(user.room).emit('message', formatMessage(botName, `${user.username} has left the chat`))
+            // Send users and room info to sidebar
+            io.to(user.room).emit('roomUsers', {
+                room: user.room,
+                users: getRoomUsers(user.room)
+            })
+    }   
+    })
+
+
+})
+
+
+const PORT = 3000 || process.env.PORT;
+
+
+
+
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
